6dcd68e42bd86a297882907c61968acefa07e39b
StringEpsilon <StringEpsilon@gmail.com>
2025-05-12 18:06:33 +0200

    Misc. small cleanups. Added dialog styling to the frontend.

--- src/Frontend/src/pages/mappers/subpages/MapperRestorePage.tsx
+++ src/Frontend/src/pages/mappers/subpages/MapperRestorePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAPI } from "../../../hooks/useAPI";
 import { Store } from "../../../utility/propertyStore";
 import { ArchivedMapper, ArchivedMappers } from "pokeaclient";
@@ -96,25 +96,29 @@ type ModalProps = {
 }
 
 function ConfirmationModal(props: ModalProps) {
+	const dialogRef = useRef<HTMLDialogElement>(null);
+	useEffect(() => {
+		if (!!dialogRef.current && props.display) {
+			dialogRef.current.showModal();
+		}
+	}, [!!dialogRef.current, props.display]);
 	if (!props.display) {
 		return null;
 	}
 	return (
-		<>
-			<div className="overlay  active"></div>
-			<dialog className="modal active no-round">
-				{props.title && <h5>{props.title}</h5>}
-				<div>{props.text}</div>
-				<nav className="right-align no-space">
-					<button className="transparent no-round link" onClick={props.onCancel}>
-						CANCEL
-					</button>
-					<button className="transparent no-round link inverse-link" onClick={props.onConfirm}>
-						{props.confirmLabel}
-					</button>
-				</nav>
-			</dialog>
-		</>
+		<dialog ref={dialogRef} onToggle={(e) => e.newState === "closed" && props.onCancel()}>
+			{props.title && <h2>{props.title}</h2>}
+			<p>{props.text}</p>
+			<div>
+				<button className="margin-right" onClick={props.onCancel}>
+					CANCEL
+				</button>
+				<button className="" onClick={props.onConfirm}>
+					{props.confirmLabel}
+				</button>
+			</div>
+		</dialog>
+
 	)
 }
 
